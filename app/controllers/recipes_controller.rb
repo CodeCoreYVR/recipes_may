d7903888d7d452d471e51955684c0b0b53f0e309
Tammam Kbeili <tammam.kbeili@gmail.com>
2015-06-10 12:03:56 -0700

    implement recipes with ingredients

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,26 @@
+class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    recipe_params = params.require(:recipe).permit(:name, :description)
+    @recipe = Recipe.new recipe_params
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @recipe = Recipe.find params[:id]
+    @ingredient = Ingredient.new
+  end
+
+end
